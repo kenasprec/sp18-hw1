e2c516749e827d69f18cd902fa86b5f6bbf93c7c
Kendal Asprec <kenasprec@berkeley.edu>
2018-02-15 17:55:33 -0800

    Finished Q1-Q4

--- foobar.rb
+++ foobar.rb
@@ -4,5 +4,7 @@ class Foobar
     # Class method, not an instance method
     # Call with `Foobar.baz`
     # Q4 CODE HERE
+    a.map(&:to_i).map {|a| a + 2}.delete_if {|a| a % 2 == 1}.delete_if {|a| a >= 10}.sum
   end
+
 end
--- hw1.rb
+++ hw1.rb
@@ -1,16 +1,16 @@
 def squared_sum(a, b)
   # Q1 CODE HERE
-
+  (a + b) * (a + b)
 end
 
 def sort_array_plus_one(a)
   # Q2 CODE HERE
-
+  a.sort.map {|a| a + 1}
 end
 
 def combine_name(first_name, last_name)
   # Q3 CODE HERE
-
+  first_name + " " + last_name
 end
 
 def blockin_time(a)
@@ -49,4 +49,5 @@ def scrabble(word)
     z: 10,
   }
   # Q5 CODE HERE
+  values.fetch_values(word.chars.map {|x| x.to_sym})
 end
